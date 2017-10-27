b02755678ca4e9fd3923e547b99da5d52ceb5f91
Dave Michelson <daveism@gmail.com>
2017-10-27 16:24:42 -0400

    update to conver to tif

---
+++ convert_imagemagik.sh
@@ -0,0 +1,23 @@
+!/bin/sh
+for f in gif/*.gif
+   do
+     echo "Processing $f"
+
+    fname=$(basename $f .gif)
+    convert $f  -type Grayscale bw/raw/$fname.gif
+
+done
+
+
+for f in png/*.png
+   do
+     echo "Processing $f"
+
+    fname=$(basename $f .png)
+    convert $f  -type Grayscale bw/raw/$fname.png
+
+done
+
+
+mogrify -format tif -path bw/ bw/raw/*.png
+mogrify -format tif -path bw/ bw/raw/*.gif
